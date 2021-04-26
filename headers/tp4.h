ffe44119ab68dd8f5b9334650c7b2ba8d603c844
Gabriel Lefloch <gabslefloch@gmail.com>
2021-04-26 23:03:12 +0200

    Fix: missing header

---
+++ headers/tp4.h
@@ -0,0 +1,13 @@
+void EcrSMO(char *filename,
+            int *NbLign,
+            float *SecMembreO,
+            int *AdPrCoLiO,
+            float *MatriceO,
+            int *NumColO);
+
+void LecSMO(char *filename,
+           int *NbLign,
+           float **SecMembreO,
+           int **AdPrCoLiO,
+           float **MatriceO,
+           int **NumColO);
