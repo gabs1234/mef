855f2ae55da0024995e56b603442cd2f679260cd
localvez <l.m.calvez@free.fr>
2021-04-26 21:03:12 +0200

    main finalisé
    (bon en même temps c'est pas testé donc on sait jamais...)

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "headers/tp2.h"
 #include "headers/tp1.h"
+#include "readNumRef.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -27,7 +28,7 @@ int main()
   int nb_vrtx_per_element;
   int **vertex_ref_array;
 
-
+  /* Lecture du fichier de maillage */
   if(readMeshFile(file_name,
                   &element_type,
                   &nb_nodes,
@@ -41,7 +42,51 @@ int main()
     printf("Malfonction de la fonction readMeshFile !\n");
     exit(1);
   }
-  else {printf("Le fichier a été lu avec succès !\n");}
+  else {printf("Le fichier de maillage a été lu avec succès !\n");}
+
+  char *ref_file_name = "NUMREF.Test";
+  int numRefDomain;
+  int N_numRefDirichletHomogene;
+  int *tab_numRefDirichletHomogene;
+  int N_numRefDirichletInhomogene;
+  int *tab_numRefDirichletInhomogene;
+  int N_numRefNeumann;
+  int *tab_numRefNeumann;
+
+  /* Lecture du fichier contenant les conditions aux limites */
+  if(readNumRef(ref_file_name,
+                &numRefDomain,
+                &N_numRefDirichletHomogene,
+                &tab_numRefDirichletHomogene,
+                &N_numRefDirichletInhomogene,
+                &tab_numRefDirichletInhomogene,
+                &N_numRefNeumann,
+                &tab_numRefNeumann))
+  {
+    printf("Malfonction de la fonction readNumRef !\n");
+    exit(1);
+  }
+  else {printf("Le fichier de référence a été lu avec succès !\n");}
+
+  /* TESTS TEMPORAIRES !*/
+  printf("===== NUMEROS DE REFERENCE =====\n");
+  printf("Numéro de référence du domaine : %d\n",numRefDomain);
+  printf("Nombre de réfénrences DH : %d\n",N_numRefDirichletHomogene);
+  printf("Références DH\n");
+  for(int z = 0; z < N_numRefDirichletHomogene ; z++)
+  {
+     printf("N°%d : %d\n",z,tab_numRefDirichletHomogene[z]);
+  }
+  printf("Références DNH\n");
+  for(int z = 0; z < N_numRefDirichletInhomogene ; z++)
+  {
+     printf("N°%d : %d\n",z,tab_numRefDirichletInhomogene[z]);
+  }
+  printf("Références Neumann\n");
+  for(int z = 0; z < N_numRefNeumann ; z++)
+  {
+     printf("N°%d : %d\n",z,tab_numRefNeumann[z]);
+  }
 
 
   /* Pour chaque élément, calcul et affichage de la matrice élémentaire */
@@ -57,31 +102,31 @@ int main()
 		    coorEl);
 
       
-      // Matrice et second menbre élémentaire
+      // Initialisation de la matrice et du second menbre élémentaire
       float **MatElem;
       MatElem = alloctab_float(nb_node_per_element, nb_node_per_element);
 
       float SMbrElem[nb_node_per_element];
 
       int NuDElem[nb_node_per_element];
+      for(int l = 0; l < nb_node_per_element; l++)
+      {
+         NuDElem[l]=1;
+      }
+      /* Condition de dirichlet au noeud i homogène NuDElem[i] = 0
+         Condition de dirichlet au noeud i inhomogène NuDElem[i] = -1
+         NuDElem[i] = 1 sinon */
       float uDElem[nb_node_per_element];
+      /* Valeur imposée par la condition de dirichlet non homogène au point i
+         (si pertinent, 0 sinon) */
 
-      // Tableaux de référence
-      int nbRefD0 = 0;
-      int numRefD0[nb_vrtx_per_element];
-      int nbRefD1 = 0;
-      int numRefD1[nb_vrtx_per_element];
-      int nbRefF1 = 0;
-      int numRefF1[nb_vrtx_per_element];
-
-
-      cal1Elem(0,
-	       nbRefD0,
-	       numRefD0,
-	       nbRefD1,
-	       numRefD1,
-	       nbRefF1,
-	       numRefF1,
+      cal1Elem(numRefDomain,
+               N_numRefDirichletHomogene,
+               tab_numRefDirichletHomogene,
+               N_numRefDirichletInhomogene,
+               tab_numRefDirichletInhomogene,
+               N_numRefNeumann,
+               tab_numRefNeumann,
 	       element_type,
 	       nb_node_per_element,
 	       coorEl,
