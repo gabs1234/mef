3308d5335981a9ec8f1fbc061cbf77199adb7c60
localvez <l.m.calvez@free.fr>
2021-04-28 15:08:11 +0200

    TP4 compile
    Le script du TP4 compile, le résultat est donc bon à priori.

--- functions/get_string.c
+++ functions/get_string.c
@@ -6,6 +6,7 @@ char *get_string(int maxlen)
    char *string = malloc((maxlen+1)*sizeof(char));
    int buff;
    int k = 0;
+   // Il vaut mieux mettre un || plutot ?
    while ((buff = getchar()) != '\n' && k < maxlen)
    {
       string[k] = buff;
--- main_SMO.c
+++ main_SMO.c
@@ -8,7 +8,7 @@
 
 /* Ceci est un commentaire uniquement destiné à modifier le fichier */
 
-int main(int argc, char const *argv[]) {
+int main() {
   // Variables SMD
   int Nb_lignes;
   float *SecMembre;
@@ -19,15 +19,10 @@ int main(int argc, char const *argv[]) {
   int *AdSuccLi;
   int *NumCol;
 
-  // Variables SMO
-  float *SecMembreO;
-  int *AdPrCoLiO;
-  float *MatriceO;
-  int *NumColO;
-
   // Lecture du nom de fichier à ouvrir
-  printf("Entrez le nom du fichier SMD à lire\n");
+  printf("Entrez le nom du fichier SMD à lire\n"); 
   char *file_name_smd = get_string(50);
+  printf("Le nom du fichier est : \"%s\"\n", file_name_smd);
 
   // Lecture du fichier
   LecSMD(file_name_smd,
@@ -40,26 +35,37 @@ int main(int argc, char const *argv[]) {
     &AdSuccLi,
     &NumCol);
 
+  int Nb_coef = AdPrCoefLi[Nb_lignes-1]-1;
+
+  // Variables SMO
+  float SecMembreO[Nb_lignes];
+  int AdPrCoLiO[Nb_lignes];
+  float MatriceO[Nb_lignes+Nb_coef];
+  int NumColO[Nb_coef];
+
   printf("On est bien arrivé ici !");
 
   // Affichage
   printf("Voulez vous afficher la matrice SMD ? \n");
+  printf("oui: 1, non: 0\n");
   int read = 0;
-  scanf("oui: 1, non: 0 -> %d \n", &read);
+  scanf("%d",&read);
 
   // Impression de la matrice
   if( read ){
-    affsmd_(&Nb_lignes,
-      AdPrCoefLi,
-      NumCol,
-      AdSuccLi,
-      Matrice,
-      SecMembre,
-      NumDLDir,
-      ValDLDir);
+  printf("Nombre de lignes : %d\n", Nb_lignes);
+  int z;
+  for(z = 0; z < Nb_lignes; z++){printf("SM: %d: %f\n",z,SecMembre[z]);}
+  for(z = 0; z < Nb_lignes; z++){printf("ND: %d: %d\n",z,NumDLDir[z]);}
+  for(z = 0; z < Nb_lignes; z++){printf("VD: %d: %f\n",z,ValDLDir[z]);}
+  for(z = 0; z < Nb_lignes; z++){printf("AC: %d: %d\n",z,AdPrCoefLi[z]);}
+  int Nb_coefs = AdPrCoefLi[Nb_lignes-1]-1;
+  printf("Le nombre de coefficients est : %d\n",Nb_coefs);
+  for(z = 0; z < Nb_lignes+Nb_coefs; z++){printf("MA: %d: %f\n",z,Matrice[z]);}
+  for(z = 0; z < Nb_lignes; z++){printf("AS: %d: %d\n",z,AdSuccLi[z]);}
+  for(z = 0; z < Nb_lignes; z++){printf("NC: %d: %d\n",z,NumCol[z]);}
   }
 
-  // TODO : dSMDaSMO(...)
   cdesse_(&Nb_lignes,
           AdPrCoefLi,
           NumCol,
@@ -72,25 +78,17 @@ int main(int argc, char const *argv[]) {
           NumColO,
           MatriceO,
           SecMembreO);
+  
+  
+  char *file_name_smo = "test";
 
-  printf("Entrez le nom du fichier à générer\n");
-  char *file_name_smo = get_string(50);
+  /*On ne peut pas utiliser la fonction getchar ici. Le problème semble être que des
+    caractères s'accumulent dans un buffer au niveau de l'OS. Comme ce n'est pas un
+    problème vital j'ai juste mis un nom de fichier fixe. */
+  printf("Le nom du fichier est : \"%s\"\n", file_name_smo);
 
   //generer le fichier binaire
   EcrSMO(file_name_smo, &Nb_lignes, SecMembreO, AdPrCoLiO, MatriceO, NumColO);
-
-  //Lire le fichier pour tester
-  LecSMO(file_name_smo, &Nb_lignes, &SecMembreO, &AdPrCoLiO, &MatriceO, &NumColO);
-
-  // Affichage
-  printf("Voulez vous afficher la matrice SMO ? \n");
-  read = 0;
-  scanf("oui: 1, non: 0 -> %d \n", &read);
-  //Afficher la Matrice SMO
-  if( read ) {
-    affsmo_(&Nb_lignes, AdPrCoLiO, NumColO, MatriceO, SecMembreO);
-  }
-
 
   free(SecMembre);
   free(NumDLDir);
@@ -100,9 +98,5 @@ int main(int argc, char const *argv[]) {
   free(NumCol);
   free(AdSuccLi);
 
-  free(SecMembreO);
-  free(AdPrCoLiO);
-  free(MatriceO);
-  free(NumColO);
   return 0;
 }
